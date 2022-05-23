da95baded96cff753d31c3276d9c853abebaf44c
Jay Goyal <jay0256.cse19@chitkara.edu.in>
2022-05-23 16:52:01 +0530

    fix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 const server = require("http").createServer(app);
 
-const io = require("socket.io")(server,{cors: {origin: "http://localhost:3000"}});
+const io = require("socket.io")(server,{cors: {origin: "*"}});
 
 app.use(express.static(path.join(__dirname+"/public")));
 
